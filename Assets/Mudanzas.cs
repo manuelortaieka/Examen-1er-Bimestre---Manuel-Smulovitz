bf7560668834959745afba72623bf083c3a5036e
Manuel Smulovitz <49439375@est.ort.edu.ar>
2025-04-28 10:15:19 -0300

    code

--- Assets/Mudanzas.cs
+++ Assets/Mudanzas.cs
@@ -23,6 +23,10 @@ public class Mudanzas : MonoBehaviour
             Debug.Log("La distancia no puede ser menor a 1 KM.");
             return;
         }
+        if (pesoDeMudanza > 10000)
+        {
+            Debug.Log("El peso de su mudanza no puede ser superior a 10000 KG.");
+        }
         if (pesoDeMudanza <= 2000)
         {
             cantHoras = 25 / distanciaEnKm;
@@ -35,15 +39,68 @@ public class Mudanzas : MonoBehaviour
                 Debug.Log("Precio total" + cantHoras + "horas a $" + precioHora + ": $" + cantHoras * 3000);
             }
             else{
-                Debug.Log("Su mudanza ")
+                Debug.Log("Precio total" + cantHoras + "horas a $" + precioHora + ": $" + cantHoras * 3000);
             }
-            Debug.Log ("Su mudanza ")
-        }
+            if (pesoDeMudanza > 2000 && pesoDeMudanza <= 5000)
+            {
+                cantHoras = 25 / distanciaEnKm;
+                Debug.Log("El vehiculo necesario para su mudanza sera el Pick Up, y dada su distancia " + distanciaEnKm + " el vehiculo durara " + 25 / distanciaEnKm + " horas.");
+                if (cantHoras > 2)
+                {
+                    Debug.Log("Su mudanza costara $9000");
+                }
+                if (distanciaEnKm > 50)
+                {
+                    cantHoras = cantHoras + 1;
+                    Debug.Log("Precio total" + cantHoras + "horas a $" + precioHora + ": $" + cantHoras * 4500);
+                }
+                else
+                {
+                    Debug.Log("Precio total" + cantHoras + "horas a $" + precioHora + ": $" + cantHoras * 4500);
+                }
+                   if (pesoDeMudanza <= 2000)
+        {
+            cantHoras = 25 / distanciaEnKm;
+            Debug.Log("El vehiculo necesario para su mudanza sera el utilitario, y dada su distancia " + distanciaEnKm + " el vehiculo durara " + 25 / distanciaEnKm + " horas.");
+            if (cantHoras > 2) {
+                Debug.Log("Su mudanza costara $6000");
+            }
+            if (distanciaEnKm > 50) {
+                cantHoras = cantHoras + 1;
+                Debug.Log("Precio total" + cantHoras + "horas a $" + precioHora + ": $" + cantHoras * 3000);
+            }
+            else{
+                Debug.Log("Precio total" + cantHoras + "horas a $" + precioHora + ": $" + cantHoras * 3000);
+            }
+                    if (pesoDeMudanza > 5000 && pesoDeMudanza <= 9000)
+                    {
+                        cantHoras = 25 / distanciaEnKm;
+                        Debug.Log("El vehiculo necesario para su mudanza sera el camion, y dada su distancia " + distanciaEnKm + " el vehiculo durara " + 25 / distanciaEnKm + " horas.");
+                        if (cantHoras > 2)
+                        {
+                            Debug.Log("Su mudanza costara $14000");
+                        }
+                        if (distanciaEnKm > 50)
+                        {
+                            cantHoras = cantHoras + 1;
+                            Debug.Log("Precio total" + cantHoras + "horas a $" + precioHora + ": $" + cantHoras * 7000);
+                        }
+                        else
+                        {
+                            Debug.Log("Precio total" + cantHoras + "horas a $" + precioHora + ": $" + cantHoras * 7000);
+                        }
 
-    }
+                    }
+                }
 
-    // Update is called once per frame
-    void Update()
+                
+
+
+
+}
+
+// Update is called once per frame
+void Update()
     {
         
     }
