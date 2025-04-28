94cb5146a29048ddba9f5a7275dce71e3c1f452c
Manuel Smulovitz <49439375@est.ort.edu.ar>
2025-04-28 10:01:26 -0300

    mitad de codigo

---
+++ Assets/Mudanzas.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mudanzas : MonoBehaviour
+{
+    public float pesoDeMudanza;
+    public float distanciaEnKm;
+    float precioHora;
+    float cantHoras;
+    float precioTotalHoras;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (pesoDeMudanza < 100)
+        {
+            Debug.Log("El peso de su mudanza debe ser superior a los 100 KG.");
+            return;
+        }
+        if (distanciaEnKm < 1)
+        {
+            Debug.Log("La distancia no puede ser menor a 1 KM.");
+            return;
+        }
+        if (pesoDeMudanza <= 2000)
+        {
+            Debug.Log("El vehiculo necesario para su mudanza sera el utilitario, y dada su distancia " + distanciaEnKm + " el vehiculo durara " + 25 / distanciaEnKm + " horas");
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
